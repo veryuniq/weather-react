9cc145c0748a0153fbef8df5ab6f19928ccd768c
veryuniq <ronithawkins@gmail.com>
2022-11-04 09:54:02 -0500

    add footer and css for footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ function App() {
       <header className="App-header">
         <Weather />
       </header>
+      <footer>
+                This was coded by{" "}
+          <a
+            href="https://rococo-cupcake-63a9d5.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Véronique
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/veryuniq/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}.
+        </footer>
     </div>
   );
 }
