bbcdbf556a882acac9082bfb77b3983a3320168f
veryuniq <ronithawkins@gmail.com>
2022-11-16 13:45:01 -0600

    Updated weather forecast
    Wrapped background to support the whole page and not repeat color.
    
    Made forecast start with the next day rather than same day.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -10,7 +10,9 @@ export default function WeatherInfo(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
+        <li className="text-capitalize">
+          {props.data.description}
+        </li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
